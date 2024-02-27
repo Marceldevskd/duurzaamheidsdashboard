7f1ec5493900da27e2eff84985e49fa18a4a24b8
Siemvdn <150660103+Siemvdn@users.noreply.github.com>
2024-02-27 10:52:24 +0100

    joke en kleur

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -4,16 +4,26 @@ import Component from "../components/component"
 type propProps ={
   hoi: string
 }
+async function joke(){
+  const response = await fetch("https://v2.jokeapi.dev/joke/Programming,Dark?type=single");
+  let data = await response.json();
+  // data = JSON.parse(data)
+  return data.joke
+}
+  
 
 export default function Home() {
   const props: propProps = {
     hoi: "doei"
   }
+  
   return (
     <main className={styles.main}>
        <div className={styles.getal}>
         <h1>
-          23
+          {
+            joke()
+          }
         </h1> 
        
       </div>

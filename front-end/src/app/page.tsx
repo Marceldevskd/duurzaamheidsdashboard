7a33aa8692998af551dff134f88c38c9eaa18225
Siemvdn <150660103+Siemvdn@users.noreply.github.com>
2024-02-28 10:53:49 +0100

    getal en zin

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,17 +1,27 @@
 import styles from "./page.module.css";
 import Component from "../components/component"
-import React from "react"; 
+import React from "react";
 
-const Home:React.FC = () => {
+const Home: React.FC = () => {
 
-  
+
   return (
     <main className={styles.main}>
-       <div className={styles.getal}>
+      <div className={styles.getal}>
         <h1>
-          hallo
-        </h1> 
-       
+
+        </h1>
+      </div>
+      <div className={styles.waterverbruik_zin}>
+        Waterverbruik van de kraan op het ICT instituutsplein:
+      </div>
+      <div className={styles.getal_liter}>
+        <div className={styles.waterverbruik_getal}>
+          56
+        </div>
+        <div className={styles.Liter}>
+          L
+        </div>
       </div>
     </main>
   );

813fcd79bb536e7abc91320f2760e43dc777a08f
rensekin <rens.ege.ekin@gmail.com>
2024-02-29 03:35:13 +0100

    laatste updates van mij en siem samengevoegd in 1 bestand.
    Ik weet niet of ik het nu goed ga pushen want er staan dingen in het rood dubbel. Kijk aub even eerst of het klopt Marcel of iemand die er wat van begrijpt

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -13,6 +13,18 @@ const Home:React.FC = () => {
           <h1> Duurzaamheidsdashboard </h1>
           </div>
         </div > 
+        {/* siem zijn code  */}
+        <div className={styles.waterverbruik_zin}>
+      Waterverbruik van de kraan op het ICT instituutsplein:
+    </div>
+    <div className={styles.getal_liter}>
+      <div className={styles.waterverbruik_getal}>
+        56
+      </div>
+      <div className={styles.Liter}>
+        L
+      </div>
+    </div>
        
       </div>
     </main>

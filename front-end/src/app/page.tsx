2d2de0b7a4affb1c859d9add7d7acd1d306e9d27
hlwalZ <ajustytmy@gmail.com>
2024-03-13 22:25:53 +0100

    Belangrijk: Localhost naar port 4000 voor backend en mongodb listen van localhost (ipv6) naar 127.0.0.1 Onbelangrijk: Timer tijdelijk uitgeschakeld.

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,40 +1,35 @@
-"use client"
+"use client";
 import styles from "./page.module.css";
-import Component from "../components/component"
+import Component from "../components/component";
 import React from "react";
-import LineChart from "../components/LineChart"
+import LineChart from "../components/LineChart";
 import Timer from "../components/Timer";
 
-
 const Home: React.FC = () => {
-
-	return (
-		<main className={styles.main}>
-			<div className={styles.getal}>
-				<div className={styles.square}>
-					<div className={styles.duurzaamheidsdashboard}>
-						<h1> Duurzaamheidsdashboard </h1>
-					</div>
-				</div >
-			</div>
-			{/* siem zijn code  */}
-			<div className={styles.Waterverbruik}>
-			<div className={styles.waterverbruik_zin}>
-			Water consumption on the institute square :
-    </div>
-    <div className={styles.getal_liter}>
-      <div className={styles.waterverbruik_getal}>
-			56
-		<Timer /> 
+  return (
+    <main className={styles.main}>
+      <div className={styles.getal}>
+        <div className={styles.square}>
+          <div className={styles.duurzaamheidsdashboard}>
+            <h1> Duurzaamheidsdashboard </h1>
+          </div>
+        </div>
       </div>
-      <div className={styles.Liter}>
-        L
+      {/* siem zijn code  */}
+      <div className={styles.Waterverbruik}>
+        <div className={styles.waterverbruik_zin}>
+          Water consumption on the institute square :
+        </div>
+        <div className={styles.getal_liter}>
+          <div className={styles.waterverbruik_getal}>
+            56
+            {/* <Timer />  */}
+          </div>
+          <div className={styles.Liter}>L</div>
+        </div>
       </div>
-    </div>
-	</div>
-		{LineChart()}
-	
-		</main>
-	);
+      {LineChart()}
+    </main>
+  );
 };
-export default Home;	
+export default Home;

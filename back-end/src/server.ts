f400cb3e37544f7ab4e1730025c3a120e02c6893
Marceldevskd <marceldevisser2006@gmail.com>
2024-04-08 15:52:38 +0200

    Added lightsensor readings api's

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 import addReading from "./routes/add-reading";
 import addSensor from "./routes/add-sensor";
 import getReadings from "./routes/get-readings";
-import middleware from "./middleware";
+import lampLight from "./routes/add-light-reading/lamp-light"
+import sunLight from "./routes/add-light-reading/sun-light"
+import middleware from "./middleware/middleware";
 import cors from 'cors';
 
 // Connect to MongoDB
@@ -11,7 +13,6 @@ mongoose
 	.connect("mongodb://localhost:27017/")
 	.then(() => console.log("Connected to MongoDB"))
 	.catch((err) => console.error("Error connecting to MongoDB:", err));
-
 
 // Express.js app
 const app = express();
@@ -21,6 +22,8 @@ app.use(cors());
 app.use("/add-reading", addReading);
 app.use("/add-sensor", addSensor);
 app.use("/get-readings", getReadings);
+app.use("/add-light-reading/lamps-on", lampLight);
+app.use("/add-light-reading/sun-light", sunLight);
 
 // Start the server
 const PORT = process.env.PORT || 4000;

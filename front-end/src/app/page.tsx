a279f3bbcee45074feda08f0ced3bae74817823e
Marceldevskd <119707306+Marceldevskd@users.noreply.github.com>
2024-04-25 10:04:47 +0200

    Cleanup of the FE

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,74 +1,43 @@
 "use client";
-import React, { Component, ErrorInfo } from "react";
+import React from "react";
 import styles from "./page.module.css";
-import Header from "../components/header/header";
-import Slideshow from "../components/slideshow/slideshow";
-import LightAdvice from "@/components/licht-advies/lightAdvice";
+import Header from "../components/Header/Header";
+import Slideshow from "../components/SlideShow/SlideShow";
+import LightAdvice from "@/components/LightAdvice/lightAdvice";
 import WaterUsage from "../components/BarChart/WaterUsage";
 
-interface HomeState {
-  hasError: boolean;
-}
-
-class Home extends Component<{}, HomeState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error: Error) {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error("Error caught in Home component:", error, errorInfo);
-    // You can log the error to an error reporting service here
-  }
-
-  render() {
-    if (this.state.hasError) {
-      // Render fallback UI if an error occurs
-      return (
-        <div className={styles.errorContainer}>
-          <h1>Something went wrong.</h1>
-          <p>Please try refreshing the page or contact support.</p>
-        </div>
-      );
-    }
-
-    // Render the main content
-    return (
-      <body>
-        <main className={styles.main}>
-          <div className={styles.row}>
-            <div className={styles.column_hu}>
+const Home: React.FC = () => {
+  return (
+    <body>
+      <main className={styles.main}>
+        <div className={styles.row}>
+          {/* <div className={styles.column_hu}>
               <img src="\Images\hu-logo.png" alt="HU" className={styles.img_hu} />
-            </div>
-            <div className={styles.column_head}>
-              <Header />
-            </div>
+            </div>  {/* <div className={styles.column_hu}>
+              <img src="\Images\hu-logo.png" alt="HU" className={styles.img_hu} />
+            </div> */}
+          <div className={styles.header}>
+            <Header />
+          </div>
+        </div>
+        <div className={styles.columnContainer}>
+          <div className={styles.waterUsageColumn}>
+            <WaterUsage />
           </div>
 
-          <div className={styles.column_content}>
-            <div className={styles.waterverbruik_vak}>
-              <WaterUsage />
-            </div>
-            <div className={styles.rightContainer}>
-              <div className={styles.lightAdvice_vak}>
-                <LightAdvice />
-              </div>
-            </div>
+          <div className={styles.lightAdviceColumn}>
+            <LightAdvice />
           </div>
+        </div>
 
-          <div className={styles.column_slides}>
-            <div className={styles.slideshowContainer}>
-              <Slideshow />
-            </div>
-          </div>
-        </main>
-      </body>
-    );
-  }
+        <div className={styles.slideshowContainer}>
+          <Slideshow />
+        </div>
+
+
+      </main>
+    </body>
+  );
 }
 
 export default Home;
--- front-end/src/components/header/header.tsx
+++ front-end/src/components/header/header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./header.module.css";
 
+import Image from 'next/image';
+
 const Header = () => {
   return (
     <div className={styles.header}>
       <div className={styles.overlay}></div>
-      <img src="\Images\banner-image.png" alt="Header" className={styles.image} />
+      <Image src="/Images/banner-image.png" alt="Header" className={styles.image} width={1920} height={200}/>
       <div className={styles.text}>
         <h1>Duurzaamheidsdashboard ICT</h1>
       </div>

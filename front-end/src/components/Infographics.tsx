6c28f1eb5b6835618191508af040eed1007221ce
rensekin <rens.ege.ekin@gmail.com>
2024-05-29 22:11:29 +0200

    infographics update
    css styling aangepast + gifs vervangen en paragraven veranderd

--- front-end/src/components/Infographics.tsx
+++ front-end/src/components/Infographics.tsx
@@ -33,6 +33,8 @@
 import React, { useState, useEffect } from "react";
 import styles from "@/components/Infographics.module.css";
 
+
+
 const InfographicsCarousel: React.FC = () => {
   const [currentInfographicIndex, setCurrentInfographicIndex] = useState(0);
 
@@ -40,7 +42,7 @@ const InfographicsCarousel: React.FC = () => {
     // Function to rotate infographics every few seconds
     const interval = setInterval(() => {
       setCurrentInfographicIndex((prevIndex) => (prevIndex + 1) % 3);
-    }, 2000); // Adjust timing as needed
+    }, 25000); // Adjust timing to 15 seconds
 
     return () => clearInterval(interval);
   }, []);
@@ -53,7 +55,7 @@ const InfographicsCarousel: React.FC = () => {
           currentInfographicIndex === 0 ? styles.active : ""
         }`}
         style={{
-          opacity: currentInfographicIndex === 0 ? 1 : 0
+          opacity: currentInfographicIndex === 0 ? 1 : 0,
         }}
       >
         <img
@@ -61,7 +63,7 @@ const InfographicsCarousel: React.FC = () => {
           alt="my gif"
           className={styles.infographicImage}
         />
-        <p className={styles.textInfoGraphic}>
+        <p className={styles.textInfoGraphic2}>
           Verdieping 4 verbruikt per maand ongeveer 56400 Liter water. Dat
           staat gelijk aan de inhoud van 1,6 gemiddelde zwembaden
         </p>
@@ -72,17 +74,17 @@ const InfographicsCarousel: React.FC = () => {
           currentInfographicIndex === 1 ? styles.active : ""
         }`}
         style={{
-          opacity: currentInfographicIndex === 1 ? 1 : 0
+          opacity: currentInfographicIndex === 1 ? 1 : 0,
         }}
       >
         <img
-          src="/Images/Emmer.png"
+          src="/Images/Emmer.gif"
           alt="Second infographic"
-          className={styles.infographicImage}
+          className={styles.infographicImage3}
         />
-          <p className={styles.textInfoGraphic2}>
+        <p className={styles.textInfoGraphic2}>
           Verdieping 4 verbruikt per maand ongeveer 56400 Liter water. Dat
-          staat gelijk aan de inhoud van 1,6 gemiddelde zwembaden
+          staat gelijk aan de inhoud van 5640 emmers
         </p>
         
         {/* Add text or other content for the second infographic */}
@@ -93,18 +95,18 @@ const InfographicsCarousel: React.FC = () => {
           currentInfographicIndex === 2 ? styles.active : ""
         }`}
         style={{
-          opacity: currentInfographicIndex === 2 ? 1 : 0
+          opacity: currentInfographicIndex === 2 ? 1 : 0,
         }}
       >
         {/* Add content for the third infographic */}
         <img
-          src="/Images/bad.png"
+          src="/Images/bad.gif"
           alt="third infographic"
           className={styles.infographicImage}
         />
-          <p className={styles.textInfoGraphic2}>
+        <p className={styles.textInfoGraphic3}>
           Verdieping 4 verbruikt per maand ongeveer 56400 Liter water. Dat
-          staat gelijk aan de inhoud van 1,6 gemiddelde zwembaden
+          staat gelijk aan de inhoud van 470 baden.
         </p>
       </div>
     </div>
@@ -112,4 +114,3 @@ const InfographicsCarousel: React.FC = () => {
 };
 
 export default InfographicsCarousel;
-

18bc0e58430b6702b58a46e7db5e3459febc3a31
hlwalZ <ajustytmy@gmail.com>
2024-07-01 16:17:56 +0200

    Lampen fix

--- back-end/src/routes/add-light-reading/lamp-light.ts
+++ back-end/src/routes/add-light-reading/lamp-light.ts
@@ -1,59 +1,63 @@
-import express, { Request, Response } from 'express';
-import Sensors from '../../models/sensorsModel';
-import { Document } from 'mongodb';
-import { SensorProps, LightReadingProps } from '../../types/sensorsTypes';
-import calculateDailyLightReadings from '../../tools/calculate-daily-light-readings';
+import express, { Request, Response } from "express";
+import Sensors from "../../models/sensorsModel";
+import { Document } from "mongodb";
+import { SensorProps, LightReadingProps } from "../../types/sensorsTypes";
+import calculateDailyLightReadings from "../../tools/calculate-daily-light-readings";
 
 const app = express.Router();
 
-app.post('/', async (req: Request, res: Response) => {
-	try {
-		const sensorName: string = req.body.sensorname as string;
-		
-		if (!sensorName) {
-			return res.status(400).json({ error: 'No sensor name received' });
-		}
+app.post("/", async (req: Request, res: Response) => {
+  try {
+    const sensorName: string = req.body.sensorname as string;
 
-		if (req.body.reading === undefined) {
-			return res.status(400).json({ error: 'No reading received' });
-		}
+    if (!sensorName || typeof sensorName !== "string") {
+      return res.status(400).json({ error: "No sensor name received" });
+    }
 
-		let sensor: SensorProps | null = await Sensors.findOne({ name: sensorName });
+    if (req.body.reading === undefined) {
+      return res.status(400).json({ error: "No reading received" });
+    }
 
-		if (!sensor) {
-			return res.status(400).json({ error: 'Invalid sensor name' });
-		}
+    let sensor: SensorProps | null = await Sensors.findOne({
+      name: sensorName,
+    });
 
-		if (sensor.type.toLowerCase() !== 'light') {
-			return res.status(400).json({ error: 'Invalid sensor type (type must be "Light")' });
-		}
+    if (!sensor) {
+      return res.status(400).json({ error: "Invalid sensor name" });
+    }
 
-		if (!sensor.lightReadings) {
-			sensor.lightReadings = {
-				totalTime: 0,
-				timer: 0,
-				lastUpdateUnix: Date.now(),
-				sunShines: false,
-				lightsOn: false,
-				perDay: [],
-			} as LightReadingProps;
-		}
+    if (sensor.type.toLowerCase() !== "light") {
+      return res
+        .status(400)
+        .json({ error: 'Invalid sensor type (type must be "Light")' });
+    }
 
-		if (!sensor.lightReadings.perDay) {
-			sensor.lightReadings.perDay = [];
-		}
+    if (!sensor.lightReadings) {
+      sensor.lightReadings = {
+        totalTime: 0,
+        timer: 0,
+        lastUpdateUnix: Date.now(),
+        sunShines: false,
+        lightsOn: false,
+        perDay: [],
+      } as LightReadingProps;
+    }
 
-		sensor.lightReadings.lightsOn = (req.body.reading === 1) as boolean;
-		sensor.lightReadings.lastUpdateUnix = Date.now();
+    if (!sensor.lightReadings.perDay) {
+      sensor.lightReadings.perDay = [];
+    }
 
-		calculateDailyLightReadings(sensor, Date.now());
+    sensor.lightReadings.lightsOn = (req.body.reading === 1) as boolean;
+    sensor.lightReadings.lastUpdateUnix = Date.now();
 
-		await (sensor as Document).save();
-		res.status(200).json(sensor.lightReadings);
-	} catch (err) {
-		console.error('Error updating data:', err);
-		res.status(500).send('Internal Server Error');
-	}
+    calculateDailyLightReadings(sensor, Date.now());
+
+    await (sensor as Document).save();
+    res.status(200).json(sensor.lightReadings);
+  } catch (err) {
+    console.error("Error updating data:", err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 export default app;
